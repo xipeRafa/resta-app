3aea8020d69f02753475cefd4fa0ac48f03597c3
rafael <rafaelsistem87@gmail.com>
2021-11-16 19:41:30 -0700

    siiu

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDMN-qASAoWNhncdKCWwgQvX_iRJYP-Zc4",
+  authDomain: "restaurant-dc4b3.firebaseapp.com",
+  projectId: "restaurant-dc4b3",
+  storageBucket: "restaurant-dc4b3.appspot.com",
+  messagingSenderId: "151109757557",
+  appId: "1:151109757557:web:c880a8840d1df6a1e7663c"
+};
+
+   export default firebaseConfig;
